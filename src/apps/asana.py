a81f2985bd346def5b13c4562694b1d3ff47230b
mutdmour <mutdmour@gmail.com>
2020-05-11 19:03:16 +0300

    add asana support

---
+++ src/apps/asana.py
@@ -0,0 +1,48 @@
+# coding: utf8
+
+shortcuts = {
+    "Asana": #https://asana.com/guide/help/faq/shortcuts
+        {
+            "Mark as Today in your My Tasks": "Tab-Y",
+            "Mark as Upcoming in your My Tasks": "Tab-U",
+            "Mark as Later in your My Tasks": "Tab-L",
+            "Move Up/Down": "K / J",
+            "Jump to Inbox tab": "H",
+            "Jump to Archive tab": "S",
+            "Follow or Unfollow": "F",
+            "Archive": "I",
+            "Move to Inbox": "U",
+            "New Task": "↵",
+            "Delete Selected Task(s)": "Tab-Backspace",
+            "Delete current task (when task name is empty)": "Backspace",
+            "Complete Selected Task(s)": "⌘↵",
+            "Move Up/Down": "⌘↑ / ⌘↓",
+            "Jump Up/Down to the next Section": "⌘⇧↑ / ⌘⇧↓",
+            "Copy Selected Tasks": "⌘C",
+            "Paste Tasks (One per line)": "⌘V",
+            "Assign to Me": "Tab-M",
+            "Access Home": "Tab-H",
+            "Create a Section": "Tab-N",
+            "Add to a project": "Tab-P",
+            "Navigate to My Tasks": "Tab-Z",
+            "Navigate to Inbox": "Tab-I",
+            "Collapse Sidebar": "Tab-O",
+            "Comment on Selected Task": "Tab-C",
+            "Assign Selected Task": "Tab-A",
+            "Set Due Date": "Tab-D",
+            "Add Follower": "Tab-F",
+            "Add a tag to the task": "Tab-T",
+            "Focus Subtasks": "Tab-S",
+            "Search for a project, tag, person, or task": "Tab-/",
+            "Change Selection": "↑ / ↓",
+            "Select Range": "⇧-Click",
+            "Select Range": "⇧↑/ ⇧↓",
+            "Multi-select Individual Task": "⌘-Click",
+            "Show Keyboard Shortcuts": "⌘/",
+            "Collapse the right pane": "ESC",
+            "Enter Focus Mode": "Tab-X",
+            "Quick Add": "Tab-Q",
+            "Post Comment (From comment field)": "Tab-↵",
+            "What it sounds like": "Tab-B"
+        }
+    }
