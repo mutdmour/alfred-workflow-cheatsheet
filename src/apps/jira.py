a8290bc9eef4cb457ac7b0a01caa8291c18130d2
Mutasem Aldmour <4711238+mutdmour@users.noreply.github.com>
2025-06-25 17:29:45 +0200

    Update shortcuts for src/apps/jira.py from https://confluence.atlassian.com/agile066/jira-agile-user-s-guide/using-keyboard-shortcuts#UsingKeyboardShortcuts-modifierkeys

--- src/apps/jira.py
+++ src/apps/jira.py
@@ -1,50 +1,53 @@
 # coding: utf8
 
 shortcuts = {
-    #https://confluence.atlassian.com/agile066/jira-agile-user-s-guide/using-keyboard-shortcuts#UsingKeyboardShortcuts-modifierkeys
-    "Jira" : {
+    "JIRA Agile (Mac)": {
         "Go to Dashboard": "g then d",
         "Browse to a Project": "g then p",
-        "Go to Agile": "g then a OR g then h",
+        "Go to Agile": "g then a or g then h",
         "Go to Issue Navigator": "g then i",
-        "Opens the Administration Search dialog box.": "g then g",
+        "Administration Search Dialog Box (if you have admin permission)": "g then g",
         "Quick Search": "/",
         "Create an Issue": "c",
         "Open shortcut help": "?",
-        "Dashboards drop-down menu": "Ctrl (+ alt for Safari) + d",
-        "Projects drop-down menu": "Ctrl (+ alt for Safari) + p",
-        "Issues drop-down menu": "Ctrl (+ alt for Safari) + i",
-        "Agile drop-down menu": "Ctrl (+ alt for Safari) + g ",
-        "Form Submit": "Ctrl (+ alt for Safari) + s ",
-        "Cancel Form ": "Ctrl (+ alt for Safari) + `",
+        "Dashboards drop-down menu": "Modifier keys + d",
+        "Projects drop-down menu": "Modifier keys + p",
+        "Issues drop-down menu": "Modifier keys + i",
+        "Agile drop-down menu": "Modifier keys + g",
+        "Form Submit": "Modifier keys + s",
+        "Cancel Form": "Modifier keys + ` (backquote)",
+        
         "Next Issue": "j",
         "Previous Issue": "k",
         "Next Column": "n",
         "Previous Column": "p",
-        "Expand/Collapse All Swimlanes ": "t",
-        "View Issue": "o   ",
+        "Hide/Show Detail View": "t",
+        "View Issue": "o",
         "Add a New Line": "Enter",
         "Submit changes": "Enter",
-        "Cancel changes": "Esc ",
+        "Cancel changes": "Esc",
         "Assign to Me": "i",
         "Send to Top": "s + t",
-        "Send to Bottom": "s + b ",
+        "Send to Bottom": "s + b",
         "Edit Issue": "e",
-        "Projector Mode": "z   ",
+        "Projector Mode": "z (press repeatedly cycles modes)",
+
         "View selected Issue": "o or Enter",
-        "Next Issue": "j",
-        "Previous Issue": "k",
         "Back to the Navigator": "u",
-        "Hide/Show Left Section": "[",
+        "Hide/Show Left Section": "[ (left square bracket)",
         "Next Activity": "n",
         "Previous Activity": "p",
         "Focus search field": "f",
         "Escape field": "Esc",
-        "Edit Issue": "e",
         "Assign Issue": "a",
         "Comment on Issue": "m",
-        "Edit Issue Labels": "l",
-        "Share Issue": "s",
-        "Operations dialog box": "."
+        "Edit Issue Labels": "l (lower-case L)",
+        "Operations dialog box": ". (period/full-stop)"
+    },
+    "Modifier keys on Mac OS X": {
+        "Firefox browser": "Ctrl",
+        "Safari browser": "Ctrl + Alt"
     }
 }
+
+page_url = "https://confluence.atlassian.com/agile066/jira-agile-user-s-guide/using-keyboard-shortcuts#UsingKeyboardShortcuts-modifierkeys"
