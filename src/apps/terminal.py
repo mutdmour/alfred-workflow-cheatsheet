1a521c9993f5e6c73fd013a5a931d71aa811646b
Mutasem Aldmour <4711238+mutdmour@users.noreply.github.com>
2025-06-25 17:29:56 +0200

    Update shortcuts for src/apps/terminal.py from https://github.com/0nn0/terminal-mac-cheatsheet

--- src/apps/terminal.py
+++ src/apps/terminal.py
@@ -1,27 +1,27 @@
 # coding: utf8
+
 shortcuts = {
-    "Terminal":{
-    #https://github.com/0nn0/terminal-mac-cheatsheet
-        "Go to the beginning of the line you are currently typing on.":"Ctrl A ",
-        "Go to the end of the line you are currently typing on.":"Ctrl E ",
-        "Clears everything on current line":"Ctrl Q ",
-        "Clears the Screen":"Ctrl L ",
-        "Clears the Screen":"⌘K",
-        "Cut everything backwards to beginning of line":"Ctrl U",
-        "Cut everything forward to end of line":"Ctrl K",
-        "Cut one word backwards using white space as delimiter":"Ctrl  W",
-        "Paste whatever was cut by the last cut command":"Ctrl Y",
-        "Same as backspace":"Ctrl H",
-        "Kill whatever you are running":"Ctrl C",
-        "Exit the current shell when no process is running, or send EOF to a the running process":"Ctrl D",
-        "Puts whatever you are running into a suspended background process. fg restores it.":"Ctrl Z",
-        "Undo the last command.":"Ctrl ⇧-",
-        "Swap the last two characters before the cursor":"Ctrl T",
-        "Move cursor one character forward":"Ctrl F",
-        "Move cursor one character backward":"Ctrl B",
-        "Move cursor one word forward":"⌥→",
-        "Move cursor one word backward":"⌥←",
-        "Swap the last two words before the cursor":"Esc T",
-        "Auto-complete files and folder names":"Tab "
-    }
+    "Mac Terminal Shortcuts": {
+        "Go to the beginning of the line": "Ctrl + A",
+        "Go to the end of the line": "Ctrl + E",
+        "Clear the screen": ["Ctrl + L", "Cmd + K"],
+        "Cut everything backwards to beginning of line": "Ctrl + U",
+        "Cut everything forward to end of line": "Ctrl + K",
+        "Cut one word backwards using white space as delimiter": "Ctrl + W",
+        "Paste whatever was cut by the last cut command": "Ctrl + Y",
+        "Same as backspace": "Ctrl + H",
+        "Kill whatever you are running and clear current line": "Ctrl + C",
+        "Exit current shell or send EOF to running process": "Ctrl + D",
+        "Suspend running process (fg restores it)": "Ctrl + Z",
+        "Undo the last command": "Ctrl + _ (Ctrl + Shift + minus)",
+        "Swap the last two characters before the cursor": "Ctrl + T",
+        "Move cursor one character forward": "Ctrl + F",
+        "Move cursor one character backward": "Ctrl + B",
+        "Move cursor one word forward": "Option + →",
+        "Move cursor one word backward": "Option + ←",
+        "Swap the last two words before the cursor": "Esc + T",
+        "Cut one word backwards using non-alphabetic delimiters": "Esc + Backspace",
+        "Auto-complete files and folder names": "Tab",
+    },
+    "Page URL": "https://github.com/0nn0/terminal-mac-cheatsheet"
 }
