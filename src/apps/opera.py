b5f1d91327b91a5af4fade0077e9b0b9e310b9c7
Mutasem Aldmour <4711238+mutdmour@users.noreply.github.com>
2025-06-25 17:29:49 +0200

    Update shortcuts for src/apps/opera.py from https://help.opera.com/en/latest/shortcuts/

--- src/apps/opera.py
+++ src/apps/opera.py
@@ -1,60 +1,70 @@
 # coding: utf8
+
 shortcuts = {
-	"Opera": {
-		#https://help.opera.com/en/latest/shortcuts/
-		"Open Downloads page": "⌘J",
-		"Open Extensions page": "⌘⇧E",
-		"Open History page": "⌘⇧H",
-		"Open local file": "⌘O",
-		"Focus address bar": "⌘L",
-		"Open Preferences (Settings) page": "⌘,",
-		"Print current page": "⌘P",
-		"Quit Opera": "⌘Q",
-		"Save a page locally": "⌘S",
-		"Find on page": "⌘F",
-		"Find next": "⌘G",
-		"Find previous": "⌘⇧G",
-		"Cycle forward through page elements": "tab",
-		"Cycle back through page elements": "SHIFT ⇧+tab",
-		"Go forward one page": "⌘→",
-		"Go back one page": "⌘←",
-		"Go to bottom of page": "⌘↓",
-		"Go to top of page": "⌘↑",
-		"Page down": "SPACE",
-		"Page up": "SHIFT ⇧+SPACE",
-		"Reload the current page": "⌘R",
-		"View current page source": "⌘U",
-		"Close current tab": "⌘W",
-		"Cycle forward through open tabs": "CTRL+tab",
-		"Cycle back through open tabs": "SHIFT ⇧+CTRL+tab",
-		"Cycle through open windows": "⌘`",
-		"Minimize a window": "⌘M",
-		"Open new tab": "⌘T",
-		"Open new window": "⌘N",
-		"Open new private window": "⌘SHIFT ⇧+N",
-		"Reopen last closed tab": "⌘SHIFT ⇧+T",
-		"Toggle tab menu": "CTRL+M",
-		"View previous active tab": "CTRL+`",
-		"Copy": "⌘C",
-		"Cut": "⌘X",
-		"Paste": "⌘V",
-		"Select all": "⌘A",
-		"Undo": "⌘Z",
-		"Redo": "⌘SHIFT ⇧+Z",
-		"Enter full-screen mode": "CTRL+SHIFT ⇧+F",
-		"Zoom in": "⌘+",
-		"Zoom out": "⌘-",
-		"Reset zoom to 100%": "⌘0",
-		"Cycle left through tabs": "1",
-		"Cycle right through tabs": "2",
-		"View previous tab": "CTRL+`",
-		"Find on page": "/",
-		"Go back one page": "Z",
-		"Go forward one page": "X",
-		"Zoom in": "+",
-		"Zoom in more": "8",
-		"Zoom out": "-",
-		"Zoom out more": "7",
-		"Reset zoom to 100%": "6"
-	}
+    "Opera Mac Keyboard Shortcuts": {
+        "COMMAND THE BROWSER": {
+            "Open Downloads page": "⌘+J",
+            "Open Extensions page": "⌘+Shift+E",
+            "Open History page": "⌘+Shift+H",
+            "Open local file": "⌘+O",
+            "Focus address bar": "⌘+L",
+            "Open Preferences (Settings) page": "⌘+,",
+            "Print current page": "⌘+P",
+            "Quit Opera": "⌘+Q",
+            "Save a page locally": "⌘+S"
+        },
+        "FIND TEXT ON THE PAGE": {
+            "Find on page": "⌘+F",
+            "Find next": "⌘+G",
+            "Find previous": "⌘+Shift+G"
+        },
+        "NAVIGATE PAGES": {
+            "Cycle forward through page elements": "Tab",
+            "Cycle back through page elements": "Shift+Tab",
+            "Go forward one page": "⌘+→",
+            "Go back one page": "⌘+←",
+            "Go to bottom of page": "⌘+↓",
+            "Go to top of page": "⌘+↑",
+            "Page down": "Space",
+            "Page up": "Shift+Space",
+            "Reload the current page": "⌘+R",
+            "View current page source": "⌘+U"
+        },
+        "CONTROL TABS AND WINDOWS": {
+            "Close current tab": "⌘+W",
+            "Cycle through open windows": "⌘+`",
+            "Minimize a window": "⌘+M",
+            "Open new tab": "⌘+T",
+            "Open new window": "⌘+N",
+            "Open new private window": "⌘+Shift+N",
+            "Reopen last closed tab": "⌘+Shift+T"
+        },
+        "MANIPULATE TEXT": {
+            "Copy": "⌘+C",
+            "Cut": "⌘+X",
+            "Paste": "⌘+V",
+            "Select all": "⌘+A",
+            "Undo": "⌘+Z",
+            "Redo": "⌘+Shift+Z"
+        },
+        "ZOOM IN AND OUT": {
+            "Enter full-screen mode": "Ctrl+⌘+F",
+            "Zoom in": "⌘++",
+            "Zoom out": "⌘+-",
+            "Reset zoom to 100%": "⌘+0"
+        },
+        "USE AND CONFIGURE ADVANCED KEYBOARD SHORTCUTS": {
+            "Cycle left through tabs": "1",
+            "Cycle right through tabs": "2",
+            "Find on page": "/",
+            "Go back one page": "Z",
+            "Go forward one page": "X",
+            "Zoom in": "+",
+            "Zoom in more": "8",
+            "Zoom out": "-",
+            "Zoom out more": "7",
+            "Reset zoom to 100%": "6"
+        }
+    },
+    "Page URL": "https://help.opera.com/en/latest/shortcuts/"
 }
