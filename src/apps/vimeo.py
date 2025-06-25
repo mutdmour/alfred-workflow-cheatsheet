cf3b5b9f8c3471568e31b5392f38fc7dbf00d173
Mutasem Aldmour <4711238+mutdmour@users.noreply.github.com>
2025-06-25 17:30:00 +0200

    Update shortcuts for src/apps/vimeo.py from https://vimeo.zendesk.com/hc/en-us/articles/115002267667-Player-keyboard-shortcuts

--- src/apps/vimeo.py
+++ src/apps/vimeo.py
@@ -1,19 +1,29 @@
 # coding: utf8
 
 shortcuts = {
-    "Vimeo": #https://vimeo.zendesk.com/hc/en-us/articles/115002267667-Player-keyboard-shortcuts
-        {
-          "Pull up the keyboard shortcuts menu": "? (Shift + /)",
-          "Volume up": "↑",
-          "Volume Down": "↓", 
-          "Scrub Forward": "→", 
-          "Scrub Backwards": "←", 
-          "Like": "L", 
-          "Share": "S", 
-          "Watch Later": "W", 
-          "Toggle Captions": "C", 
-          "Toggle Player Preferences Menu": "H", 
-          "Toggle Fullscreen": "F", 
-          "Debug Panel for technical issues": "D"
-        }
+    "Vimeo Player Keyboard Shortcuts (Mac)": {
+        "Pull up the keyboard shortcuts menu": "Shift + /",
+        "Volume up": "Shift + Volume up",
+        "Volume down": "Shift + Volume down",
+        "Scrub forward": "Right arrow",
+        "Scrub backwards": "Left arrow",
+        "Scrub backwards (alternate)": "J",
+        "Pause/Play": "K",
+        "Scrub forward (alternate)": "L",
+        "Share": "S",
+        "Watch Later": "W",
+        "View on Vimeo": "V",
+        "Toggle mute": "M",
+        "Take screenshots of the video frame": "P",
+        "Toggle captions": "C",
+        "Toggle transcript": "Shift + T",
+        "Toggle player preferences menu": "H",
+        "Toggle fullscreen": "F",
+        "Debug Panel for technical issues": "D",
+        "Frame Backward": "Shift + Left arrow",
+        "Frame Forward": "Shift + Right arrow",
+        "Pause/Play (alternate)": "Space bar"
+    }
 }
+
+page_url = "https://vimeo.zendesk.com/hc/en-us/articles/115002267667-Player-keyboard-shortcuts"
