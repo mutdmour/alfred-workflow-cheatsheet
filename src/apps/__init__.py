fa67fca00c7552799918bf3d95d589b96983b4b4
Mutasem Aldmour <4711238+mutdmour@users.noreply.github.com>
2025-06-25 16:46:18 +0200

    Update shortcuts for src/apps/__init__.py from https://www.amazon.com/All-New-Kindle-Paperwhite-Waterproof-Storage/dp/B0B3MZ9W62

---
+++ src/apps/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf8
+
+shortcuts = {
+    "Amazon Kindle Paperwhite": {
+        # No Mac shortcuts mentioned on the provided page
+    }
+}
+
+page_url = "https://www.amazon.com/All-New-Kindle-Paperwhite-Waterproof-Storage/dp/B0B3MZ9W62"
